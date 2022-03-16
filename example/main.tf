5f2015fc771dacda321b8ccdd5ae58968977e757
Adan <adan@cloudandthings.io>
2022-03-16 08:26:36 +0200

    CHORE: scrubbed Slack URL entries

--- example/main.tf
+++ example/main.tf
@@ -2,6 +2,6 @@ module "billing_notifier_account_1" {
   source = "../"
 
   naming_prefix     = local.naming_prefix
-  slack_webhook_url = "https://hooks.slack.com/services/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"
-  account_name      = "pattern_demo"
+  slack_webhook_url = "https://api.slack.com/messaging/webhooks"
+  account_name      = "cloudandthings - master"
 }
