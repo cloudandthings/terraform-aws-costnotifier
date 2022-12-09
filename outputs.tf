e75cf03af0d17de1d43f6860179c584d5f06bcb8
Velisa <velisa@cloudandthings.io>
2022-12-09 10:08:35 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "cost_notfier_lambda_name" {
   description = "Created lambda's name"
   value       = module.billing_notifier_lambda.lambda_function.function_name
 }
+
+variable "test-variable" {
+  
+}
